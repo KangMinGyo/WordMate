f58173e182352202b8e356641c75c997261fdd08
KangMinGyo <kang15567@kakao.com>
2024-11-14 15:05:45 +0900

    feat: add WordListViewController

---
+++ WordMate/Presentation/WordList/View/WordListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WordListViewController.swift
+//  WordMate
+//
+//  Created by KangMingyo on 11/14/24.
+//
+
+import UIKit
+
+class WordListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
