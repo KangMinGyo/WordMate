df5881e502325acb49490e0c64c5dd2ab91f195c
KangMinGyo <kang15567@kakao.com>
2024-11-14 16:50:42 +0900

    feat: add TableView to WordListVC

--- WordMate/Presentation/WordList/View/WordListViewController.swift
+++ WordMate/Presentation/WordList/View/WordListViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class WordListViewController: UIViewController {
 
+   var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        tableView.dataSource = self
+//        tableView.delegate = self
+        tableView.register(WordCell.self, forCellReuseIdentifier: "WordCell")
+        view.addSubview(tableView)
     }
 
 }
+
+extension WordListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath) as! WordCell
+        return cell
+    }
+    
+}
+
