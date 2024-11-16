e862baf37f4b62b6001d56203e9b18d0cc6a4f52
KangMinGyo <kang15567@kakao.com>
2024-11-16 23:26:58 +0900

    feat: add Models

---
+++ WordMate/Models/VocabularyGroup.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyGroup.swift
+//  WordMate
+//
+//  Created by KangMingyo on 11/16/24.
+//
+
+import Foundation
+
+struct VocabularyGroup {
+    let name: String
+    let language: String
+    var words: [VocabularyWord]
+}
---
+++ WordMate/Models/VocabularyWord.swift
@@ -0,0 +1,15 @@
+//
+//  VocabularyWord.swift
+//  WordMate
+//
+//  Created by KangMingyo on 11/16/24.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let pronunciation: String?
+    let meaning: String
+    let description: String?
+}
