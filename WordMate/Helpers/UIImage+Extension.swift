76ef7512989d5e29c79e73736739a93b85b7008a
KangMinGyo <kang15567@kakao.com>
2024-11-28 00:19:58 +0900

    design: add Image for LearningView

---
+++ WordMate/Helpers/UIImage+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIImage+Extension.swift
+//  WordMate
+//
+//  Created by KangMingyo on 11/27/24.
+//
+
+import UIKit
+
+extension UIImage {
+    static let flashCardImage = UIImage(named: "flashCard")
+    static let gridImage = UIImage(named: "grid")
+    static let dictationImage = UIImage(named: "dictation")
+    static let loopImage = UIImage(named: "loop")
+}
--- WordMate/Presentation/Learning/View/LearningViewController.swift
+++ WordMate/Presentation/Learning/View/LearningViewController.swift
@@ -17,52 +17,54 @@ class LearningViewController: UIViewController {
         $0.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
     }
     
-    private lazy var game1Button = UIButton().then {
-        $0.setTitle("플래시카드", for: .normal)
-        $0.backgroundColor = .systemGray3
-        $0.layer.shadowColor = UIColor.systemGray.cgColor
-        $0.layer.shadowOpacity = 0.5
-        $0.layer.shadowRadius = 5
-        $0.layer.shadowOffset = CGSize(width: 1, height: 1)
-        $0.layer.cornerRadius = 25
+    private func customButton(title: String, image: UIImage?) -> UIButton {
+        return UIButton().then {
+            $0.setTitle(title, for: .normal)
+            $0.setImage(image, for: .normal)
+            $0.backgroundColor = .systemGray6
+            $0.layer.shadowColor = UIColor.systemGray.cgColor
+            $0.layer.shadowOpacity = 0.5
+            $0.layer.shadowRadius = 5
+            $0.layer.shadowOffset = CGSize(width: 1, height: 1)
+            $0.layer.cornerRadius = 25
+        }
     }
     
-    private lazy var game2Button = UIButton().then {
-        $0.setTitle("사지선다", for: .normal)
-        $0.backgroundColor = .systemGray3
-        $0.layer.shadowColor = UIColor.systemGray.cgColor
-        $0.layer.shadowOpacity = 0.5
-        $0.layer.shadowRadius = 5
-        $0.layer.shadowOffset = CGSize(width: 1, height: 1)
-        $0.layer.cornerRadius = 25
-    }
+    private lazy var flashCardButton: UIButton = {
+        return customButton(title: "플래시카드", image: .flashCardImage)
+    }()
     
-    private lazy var game3Button = UIButton().then {
-        $0.setTitle("받아쓰기", for: .normal)
-        $0.backgroundColor = .systemGray3
-        $0.layer.shadowColor = UIColor.systemGray.cgColor
-        $0.layer.shadowOpacity = 0.5
-        $0.layer.shadowRadius = 5
-        $0.layer.shadowOffset = CGSize(width: 1, height: 1)
-        $0.layer.cornerRadius = 25
-    }
+    private lazy var choicesButton: UIButton = {
+        return customButton(title: "사지선다", image: .gridImage)
+    }()
     
-    private lazy var game4Button = UIButton().then {
-        $0.setTitle("깜빡이", for: .normal)
-        $0.backgroundColor = .systemGray3
-        $0.layer.shadowColor = UIColor.systemGray.cgColor
-        $0.layer.shadowOpacity = 0.5
-        $0.layer.shadowRadius = 5
-        $0.layer.shadowOffset = CGSize(width: 1, height: 1)
-        $0.layer.cornerRadius = 25
-    }
+    private lazy var dictationButton: UIButton = {
+        return customButton(title: "받아쓰기", image: .dictationImage)
+    }()
+    
+    private lazy var loopButton: UIButton = {
+        return customButton(title: "반복하기", image: .loopImage)
+    }()
+    
+    private lazy var firstStackView = UIStackView(arrangedSubviews: [
+        flashCardButton, choicesButton]).then {
+            $0.axis = .horizontal
+            $0.spacing = 15
+            $0.distribution = .fillEqually
+        }
+    
+    private lazy var secondStackView = UIStackView(arrangedSubviews: [
+        dictationButton, loopButton]).then {
+            $0.axis = .horizontal
+            $0.spacing = 15
+            $0.distribution = .fillEqually
+        }
     
     private lazy var stackView = UIStackView(arrangedSubviews: [
-        game1Button, game2Button, game3Button, game4Button]).then {
+        firstStackView, secondStackView]).then {
             $0.axis = .vertical
             $0.spacing = 15
             $0.distribution = .fillEqually
-            $0.alignment = .fill
         }
     
     private let buttonHeight: CGFloat = 100
