df44dd71ee60b69c5e5cdaa806cfc1dc7d3ac9fb
KangMinGyo <kang15567@kakao.com>
2024-11-29 20:15:33 +0900

    feat: add MultipleChoice game

---
+++ WordMate/Presentation/Games/MultipleChoice/MultipleChoiceViewController.swift
@@ -0,0 +1,97 @@
+//
+//  MultipleChoiceViewController.swift
+//  WordMate
+//
+//  Created by KangMingyo on 11/29/24.
+//
+
+import UIKit
+
+class MultipleChoiceViewController: UIViewController {
+    
+    private let wordLabelView = UIView().then {
+        $0.backgroundColor = .white
+    }
+    
+    private let wordLabel = UILabel().then {
+        $0.text = "Word"
+        $0.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+    }
+    
+    private let choice1Button = UIButton().then {
+        $0.tintColor = .white
+        $0.backgroundColor = .primaryOrange
+    }
+    
+    private let choice2Button = UIButton().then {
+        $0.tintColor = .white
+        $0.backgroundColor = .primaryOrange
+    }
+    
+    private let choice3Button = UIButton().then {
+        $0.tintColor = .white
+        $0.backgroundColor = .primaryOrange
+    }
+    
+    private let choice4Button = UIButton().then {
+        $0.tintColor = .white
+        $0.backgroundColor = .primaryOrange
+    }
+    
+    private lazy var stackView = UIStackView(arrangedSubviews: [
+        choice1Button, choice2Button, choice3Button, choice4Button]).then {
+            $0.axis = .vertical
+            $0.spacing = 20
+            $0.distribution = .fillEqually
+        }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupButtons()
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    private func setupButtons() {
+        // 버튼 공통 설정
+        let buttons = [choice1Button, choice2Button, choice3Button, choice4Button]
+        
+        for (index, button) in buttons.enumerated() {
+            button.setTitle("Choice \(index + 1)", for: .normal)
+            button.addTarget(self, action: #selector(handleButtonTap(_:)), for: .touchUpInside)
+            button.tag = index
+            view.addSubview(button)
+            // 레이아웃 설정 (Auto Layout 사용)
+        }
+    }
+    
+    @objc private func handleButtonTap(_ sender: UIButton) {
+        let selectedIndex = sender.tag
+        print("Selected choice: \(selectedIndex + 1)")
+    }
+        
+    private func setupSubviews() {
+        view.addSubview(wordLabelView)
+        wordLabelView.addSubview(wordLabel)
+        view.addSubview(stackView)
+    }
+
+    private func setupConstraints() {
+        wordLabelView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.centerY.equalToSuperview()
+        }
+        
+        wordLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(wordLabelView.snp.bottom).inset(20)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+    }
+}
