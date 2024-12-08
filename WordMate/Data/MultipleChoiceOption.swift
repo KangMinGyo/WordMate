20dc9aa9033168da5ee72d3b595f620fbed6ad33
KangMinGyo <kang15567@kakao.com>
2024-12-08 22:45:54 +0900

    feat: add MultipleChoiceOption for MultipleChoice learning

---
+++ WordMate/Data/MultipleChoiceOption.swift
@@ -0,0 +1,13 @@
+//
+//  MultipleChoiceOption.swift
+//  WordMate
+//
+//  Created by KangMingyo on 12/8/24.
+//
+
+import Foundation
+
+struct MultipleChoiceOption {
+    let meaning: String
+    let isCorrct: Bool
+}
