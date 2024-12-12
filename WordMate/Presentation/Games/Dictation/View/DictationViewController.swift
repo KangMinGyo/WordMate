76c562f483880019abf9777baaa772880002fdb1
KangMinGyo <kang15567@kakao.com>
2024-12-12 21:31:58 +0900

    feat: add DictationVC

---
+++ WordMate/Presentation/Games/Dictation/View/DictationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DictationViewController.swift
+//  WordMate
+//
+//  Created by KangMingyo on 12/12/24.
+//
+
+import UIKit
+
+class DictationViewController: UIViewController {
+    
+    private var options: [MultipleChoiceOption] = []
+    
+    private let gameStatusView = GameStatusView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
