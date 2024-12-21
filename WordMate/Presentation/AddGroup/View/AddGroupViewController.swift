4176bbdf58c4d4618850d7111205ac9e8d43d98b
KangMinGyo <kang15567@kakao.com>
2024-12-21 19:51:58 +0900

    feat: Text 입력 되어야만 버튼 활성화

--- WordMate/Presentation/AddGroup/View/AddGroupViewController.swift
+++ WordMate/Presentation/AddGroup/View/AddGroupViewController.swift
@@ -23,12 +23,14 @@ class AddGroupViewController: UIViewController {
         $0.backgroundColor = .systemGray6
         $0.layer.cornerRadius = 20
         $0.clipsToBounds = true
+        $0.addPadding()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        groupTextField.delegate = self
         setupNaviBar()
         setupSubviews()
         setupConstraints()
@@ -38,7 +40,8 @@ class AddGroupViewController: UIViewController {
         title = "그룹"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveButtonTapped))
         navigationController?.navigationBar.tintColor = .black
-        navigationItem.rightBarButtonItem?.tintColor = .primaryOrange
+        navigationItem.rightBarButtonItem?.tintColor = .systemGray
+        navigationItem.rightBarButtonItem?.isEnabled = false
     }
     
     @objc func saveButtonTapped() {
@@ -62,5 +65,21 @@ class AddGroupViewController: UIViewController {
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
             $0.height.equalTo(40)
         }
+    }
+}
+
+extension AddGroupViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: string)
+
+        DispatchQueue.main.async {
+            let saveButton = self.navigationItem.rightBarButtonItem
+            let isTextValid = !updatedText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+            saveButton?.isEnabled = isTextValid
+            saveButton?.tintColor = isTextValid ? .primaryOrange : .lightGray
+        }
+        
+        return true
     }
 }
